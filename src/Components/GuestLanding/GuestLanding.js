0dc2a502516ecc5665137ffccf4369612b7b5aef
Tramy Nguyen <tramynn2@gmail.com>
2019-10-13 16:37:17 -0500

    styled guest landing bg and card

--- src/Components/GuestLanding/GuestLanding.js
+++ src/Components/GuestLanding/GuestLanding.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
+import "../../images/helo_logo.png";
 
 class GuestLanding extends Component {
   render() {
     return (
-      <div>
-        <h1>Guest Landing</h1>
+      <div className="GuestLanding-container">
+        <div className="GuestLanding-card">
+            <img src={`../../images/helo_logo.png`} alt="Helo logo" />
+            <h1>Helo</h1>
+            <form>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <label>Username:</label>
+                    </td>
+                    <td>
+                      <input placeholder="Enter username.." />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label>Password:</label>
+                    </td>
+                    <td>
+                      <input placeholder="Enter password.." />
+                    </td>
+                  </tr>
+                  <button>Login</button>
+                  <button>Register</button>
+                </tbody>
+              </table>
+            </form>
+        </div>
       </div>
     );
   }
