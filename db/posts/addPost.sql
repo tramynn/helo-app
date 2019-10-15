af8ebb9c5993ebc7c4fafbee1879014870fc7d25
Tramy Nguyen <tramynn2@gmail.com>
2019-10-15 12:31:24 -0500

    created addPost sql

---
+++ db/posts/addPost.sql
@@ -0,0 +1,4 @@
+INSERT INTO posts
+(user_id, title, img, content)
+VALUES
+($1, $2, $3, $4)
--- db/seed.sql
+++ db/seed.sql
@@ -6,3 +6,22 @@ CREATE TABLE helo_user (
 );
 
 -- Create posts table
+CREATE TABLE posts (
+post_id SERIAL NOT NULL,
+title VARCHAR(100) NOT NULL,
+img TEXT,
+content VARCHAR(500) NOT NULL,
+user_id INTEGER NOT NULL REFERENCES helo_user(user_id)
+)
+
+-- Add post
+INSERT INTO posts
+(user_id, title, img, content)
+VALUES
+($1, $2, $3, $4)
+
+-- Dummy data
+INSERT INTO posts
+(user_id, title, img, content)
+VALUES
+(1, 'Today is a great day', 'https://i.pinimg.com/originals/f2/ce/c9/f2cec98f06e8f66ff0bcfb2ffdb413eb.jpg', 'Heres a pic of a cute happy cat')
